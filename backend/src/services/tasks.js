5a855833945b6e391f2b7a7945b67477e8b4082d
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-17 17:36:32 -0300

    feat(service): created update method

--- backend/src/services/tasks.js
+++ backend/src/services/tasks.js
@@ -3,9 +3,11 @@ import TaskModel from '../models/tasks.js';
 const create = async (body) => TaskModel.create(body);
 const find = async () => TaskModel.find();
 const findOne = async (id) => TaskModel.findOne(id);
+const update = async (id) => TaskModel.updateOne(id);
 
 export default {
   create,
   find,
   findOne,
+  update,
 };
