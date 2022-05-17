ae83ef8ba0d34b75b0dd0c65e32937818c6acffe
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-17 16:59:59 -0300

    feat(service): created find method

--- backend/src/services/tasks.js
+++ backend/src/services/tasks.js
@@ -1,7 +1,9 @@
 import TaskModel from '../models/tasks.js';
 
 const create = async (body) => TaskModel.create(body);
+const find = async () => TaskModel.find();
 
 export default {
   create,
+  find,
 };
