c9314fdcf1ebc3efc871be5122a8935b7ddf9a44
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-17 17:12:43 -0300

    feat(service): created findOne method

--- backend/src/services/tasks.js
+++ backend/src/services/tasks.js
@@ -2,8 +2,10 @@ import TaskModel from '../models/tasks.js';
 
 const create = async (body) => TaskModel.create(body);
 const find = async () => TaskModel.find();
+const findOne = async (id) => TaskModel.findOne(id);
 
 export default {
   create,
   find,
+  findOne,
 };
