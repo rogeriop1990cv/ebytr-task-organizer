ad2c2ce3bfa2354fa7ecedbcaf170f4030f26713
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-18 01:05:12 -0300

    test(unit/controler): creating a test of the post method

--- backend/test/unit/controllers/tasks_spec.js
+++ backend/test/unit/controllers/tasks_spec.js
@@ -3,30 +3,51 @@ import Sinon from 'sinon';
 import TaskController from '../../../src/controller/tasks.js';
 import TaskService from '../../../src/service/tasks.js';
 
-const mockResultGet = {
+const mockResultGet = [{
   _id: '62833332ac5fa02948326758',
   description: 'a simple description for the test of creating a new record in the database',
   status: 'Pendente',
   createAt: '2022-05-17T05:31:30.732Z',
   __v: 0,
+}];
+
+const mockResultPost = mockResultGet[0];
+
+const mockResultBody = {
+  description: 'a simple description for the test of creating a new record in the database',
+  status: 'Pendente',
+  createAt: '2022-05-17T05:31:30.732Z',
+};
+
+const mockResponse = () => {
+  const res = {};
+  res.status = Sinon.stub().returns(res);
+  res.json = Sinon.stub().returns(res);
+  return res;
 };
 
 describe('Task Controller Unit Testing', () => {
-  it('should get method return all task', async () => {
+  it('should "GET /" method return all task', async () => {
     Sinon.stub(TaskService, 'find').resolves(mockResultGet);
-    const req = {};
-    const mockResponse = () => {
-      const res = {};
-      res.status = Sinon.stub().returns(res);
-      res.json = Sinon.stub().returns(res);
-      return res;
-    };
-
+    const req = { };
     const res = mockResponse();
 
     await TaskController.get(req, res);
 
     expect(res.status.calledWith(200)).to.be.equal(true);
     expect(res.json.calledWith(mockResultGet)).to.be.equal(true);
+    Sinon.restore();
+  });
+
+  it('should "POST /" method return one task', async () => {
+    Sinon.stub(TaskService, 'create').resolves(mockResultPost);
+    const req = { body: mockResultBody };
+    const res = mockResponse();
+
+    await TaskController.post(req, res);
+
+    expect(res.status.calledWith(201)).to.be.equal(true);
+    expect(res.json.calledWith(mockResultPost)).to.be.equal(true);
+    Sinon.restore();
   });
 });
