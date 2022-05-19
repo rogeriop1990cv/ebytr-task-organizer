c5d8017766559c788b9e737af0a2f2b88a62192f
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-19 02:45:05 -0300

    fix(backend/api/app): added project backend cors dependency

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import tasksRouter from '../router/tasks.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(tasksRouter);
 
