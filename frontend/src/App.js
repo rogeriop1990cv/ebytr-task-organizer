cbd9e904bb8110663e38756105f831dc0c22bf2f
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-19 01:11:34 -0300

    chore: add some simple modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import CardDescription from './components/CardDescription';
+import CardInfo from './components/CardInfo';
+import CardToDo from './components/CardToDo';
 import Header from './components/Header';
 
 function App() {
   return (
     <div>
       <Header />
+      <br />
+      <CardToDo>
+        <CardDescription description="Hello My Friend!!!" />
+        <CardInfo date="20/15/1254" status="Pendente" />
+      </CardToDo>
     </div>
   );
 }
