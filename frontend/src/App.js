06736e87678a433f549f8e416ad938dc371c2325
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-19 02:08:42 -0300

    style: modifying app and hearder component CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,19 @@ import CardDescription from './components/CardDescription';
 import CardInfo from './components/CardInfo';
 import CardToDo from './components/CardToDo';
 import Header from './components/Header';
+import SortBar from './components/SortBar';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <br />
-      <CardToDo>
-        <CardDescription description="Hello My Friend!!!" />
-        <CardInfo date="20/15/1254" status="Pendente" />
-      </CardToDo>
+      <SortBar />
+      <div className="container__cards">
+        <CardToDo>
+          <CardDescription description="Hello My Friend!!!" />
+          <CardInfo date="20/15/1254" status="Pendente" />
+        </CardToDo>
+      </div>
     </div>
   );
 }
