d331c87d7048da334ea9d7d9dff706d9ae772d60
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-05-19 02:47:52 -0300

    feat(src/component): adding useEffect e useState in the App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CardDescription from './components/CardDescription';
 import CardInfo from './components/CardInfo';
 import CardToDo from './components/CardToDo';
 import Header from './components/Header';
+import ImgAddToDo from './components/ImgAddToDo';
 import SortBar from './components/SortBar';
 
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const allTodo = await fetch('http://localhost:3001/');
+      const dataJson = await allTodo.json();
+      setToDoList(dataJson);
+    })();
+  }, []);
+
   return (
     <div className="container">
       <Header />
       <SortBar />
       <div className="container__cards">
+        {/* Card de add task */}
         <CardToDo>
-          <CardDescription description="Hello My Friend!!!" />
-          <CardInfo date="20/15/1254" status="Pendente" />
+          <ImgAddToDo />
+          <CardInfo date="" status="Adicionar" />
         </CardToDo>
+
+        {
+          toDoList.map((task) => (
+            <CardToDo key={task._id}>
+              <CardDescription description={task.description} />
+              <CardInfo date={task.createAt} status={task.status} />
+            </CardToDo>
+          ))
+        }
+
       </div>
     </div>
   );
