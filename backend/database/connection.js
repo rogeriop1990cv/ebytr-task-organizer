8b219b8d3a0d62202c52a1b1b16f647b9cfca7ce
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2022-07-21 23:27:15 -0300

    refactor: added online database and fixing code execution script

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 async function connection(database = 'task') {
-  return mongoose.connect(`mongodb://localhost:27017/${database}`);
+  return mongoose.connect(process.env.DB || `mongodb://localhost:27017/${database}`);
 }
 
 export default connection;
